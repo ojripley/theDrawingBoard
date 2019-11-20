662d68bf96b8e6c7a73386ed420b2abde41ce72c
Owen Ripley <ojripley@gmail.com>
2019-11-20 10:37:25 -0500

    add Contact.js component return

--- client/src/components/contacts/Contact.js
+++ client/src/components/contacts/Contact.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export default function Contact(props) {
-
-  return
+  return(
+    <>
+      <li className='contact-card'>
+        <span>
+          <h3>{props.username}</h3>
+          <p>{props.email}</p>
+        </span>
+      </li>
+    </>
+  );
 }
