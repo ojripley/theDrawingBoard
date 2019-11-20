1038940316db2a70291caeac6bc8a273506e21e1
Owen Ripley <ojripley@gmail.com>
2019-11-20 10:06:55 -0500

    rename notes in schema

--- backend/db/schema/04_users_meetings.sql
+++ backend/db/schema/04_users_meetings.sql
@@ -3,6 +3,6 @@ CREATE TABLE users_meetings (
   id SERIAL PRIMARY KEY NOT NULL,
   meeting_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
-  link_to_notes VARCHAR(255),
+  notes VARCHAR(255),
   attendance VARCHAR(255)
 );
