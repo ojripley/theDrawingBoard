4a300428ac17e3f4d769c79acf72d4980ebf1ac4
Owen Ripley <ojripley@gmail.com>
2019-11-20 17:14:41 -0500

    add search bar to contacts page

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Contact from './Contact';
 
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 
 // boiled data
@@ -19,9 +22,27 @@ const friends = [
   },
 ];
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
 
 
 export default function Contacts(props) {
+  const classes = useStyles();
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearchTermChange = event => {
+    setSearchTerm = (event.target.value);
+  }
 
   const contacts = friends.map(friend =>
     <Contact
@@ -34,6 +55,15 @@ export default function Contacts(props) {
   return (
     <>
       <h1>Contacts</h1>
+        <TextField
+          id="outlined-name"
+          label="Contacts"
+          className={classes.textField}
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+          margin="normal"
+          variant="outlined"
+        />
       <ul>
         {contacts}
       </ul>
