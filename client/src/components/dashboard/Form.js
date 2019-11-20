19799cd220176113014636a9dd13c0ae5fb78e39
Tammie Chung <tammie.chung11@gmail.com>
2019-11-20 14:53:55 -0500

    Add form to Dashboard

--- client/src/components/dashboard/Form.js
+++ client/src/components/dashboard/Form.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
 
+// Material UI - Text Inputs
+import TextField from '@material-ui/core/TextField';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+// Material UI - Date & Time Pickers
 import 'date-fns';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
@@ -10,6 +13,17 @@ import {
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+
+// Material UI - Name Selector
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Chip from '@material-ui/core/Chip';
+
+// Material UI - Button
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -22,12 +36,69 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(1),
     width: 200,
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: 2,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
 }));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
+
 
 export default function Form() {
   const classes = useStyles();
+  const theme = useTheme();
 
-  const [selectedDate, setSelectedDate] = React.useState(new Date);
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = event => {
+    setPersonName(event.target.value);
+  };
 
   const handleDateChange = date => {
     setSelectedDate(date);
@@ -55,7 +126,7 @@ export default function Form() {
             <KeyboardDatePicker
               margin="normal"
               id="date-picker-dialog"
-              label="Date"
+              label="date"
               format="MM/dd/yyyy"
               value={selectedDate}
               onChange={handleDateChange}
@@ -76,6 +147,34 @@ export default function Form() {
           </Grid>
         </MuiPickersUtilsProvider>
       </div>
+      <div>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-chip-label">Contacts</InputLabel>
+          <Select
+            labelId="demo-mutiple-chip-label"
+            id="demo-mutiple-chip"
+            multiple
+            value={personName}
+            onChange={handleChange}
+            input={<Input id="select-multiple-chip" />}
+            renderValue={selected => (
+              <div className={classes.chips}>
+                {selected.map(value => (
+                  <Chip key={value} label={value} className={classes.chip} />
+                ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
+          >
+            {names.map(name => (
+              <MenuItem key={name} value={name} style={getStyles(name, personName, theme)}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <Button className={classes.button} variant='contained' color='primary'>Submit</Button>
     </form>
   );
 }
--- client/src/components/dashboard/Owner.js
+++ client/src/components/dashboard/Owner.js
@@ -3,10 +3,19 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 export default function Owner(props) {
+
+  const onDestroy = () => {
+    console.log('destroy')
+  };
+
+  const onEdit = () => {
+    console.log('edit')
+  };
+
   return (
     <div>
-      <EditIcon />
-      <DeleteIcon />
+      <EditIcon onClick={onEdit}/>
+      <DeleteIcon onClick={onDestroy}/>
     </div>
   );
 }
