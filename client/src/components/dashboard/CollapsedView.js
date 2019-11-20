cb8e8ef2b7a48ece8ea87ae037bef1ee3a788530
Tammie Chung <tammie.chung11@gmail.com>
2019-11-20 09:35:26 -0500

    Replace expandable list with card component

---
+++ client/src/components/dashboard/CollapsedView.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+    border: 'solid 1px black'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function CollapsedView() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h5" component="h2">Date/Time</Typography>
+        <Typography className={classes.pos} color="textSecondary">Meeting Name</Typography>
+        <Typography variant="body2" component="p">Owner Name</Typography>
+        <Typography variant="body2" component="p"># of Attendees</Typography>
+      </CardContent>
+    </Card>
+  );
+}
