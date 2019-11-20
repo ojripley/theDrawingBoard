3d600c70fd72306c1c070117e25048a053b210ec
Thilakshan <th_thilakshan@hotmail.com>
2019-11-20 08:38:49 -0500

    moved state to app level for switching tabs

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,33 @@ import TabBar from './TabBar';
 
 export default function App() {
   // const LOGIN = 'LOGIN';
+  const loggedIn = true;//Change this :)
   const DASHBOARD = 'DASHBOARD';
   const HISTORY = 'HISTORY';
   const CONTACTS = 'CONTACTS';
   const ACTIVE = 'ACTIVE'
 
   const [mode, setMode] = useState(DASHBOARD);
+  //top nav
+  //login page if not logged in
+  //dashboard if logged in
 
-  return (
-    //top nav
-    //login page if not logged in
-    //dashboard if logged in
-    <Box>
-      <TabBar />
-    </Box>
-  );
+  if (loggedIn) {
+    return (
+      //If logged in show dashboard
+      <Box>
+        {mode === DASHBOARD && <Dashboard />}
+        {mode === HISTORY && <History />}
+        {mode === CONTACTS && <Contacts />}
+        {mode === ACTIVE && <Active />}
+        <TabBar mode={mode} setMode={setMode} />
+      </Box >
+
+    );
+  } else {
+    return (
+      <h1> Replace with login page </h1>
+    );
+
+  }
 }
