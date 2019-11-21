08f8dd8e27ed31f13a2863b85184257d09652b0e
Tammie Chung <tammie.chung11@gmail.com>
2019-11-21 09:21:47 -0500

    Fix expanded state bug

--- client/src/components/dashboard/MeetingCard.js
+++ client/src/components/dashboard/MeetingCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
