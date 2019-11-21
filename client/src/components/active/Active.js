4c2fa028c651363edf0719ea4a0f4896dfaae9b5
Thilakshan <th_thilakshan@hotmail.com>
2019-11-21 08:19:58 -0500

    add styling

--- client/src/components/active/Active.js
+++ client/src/components/active/Active.js
@@ -5,7 +5,11 @@ import ImageCanvas from './Image';
 
 export default function Active({ socket, socketOpen }) {
   const [isLoaded, setLoaded] = useState(false);
+  //TODO: Get upcoming meetings
+  const [inMeeting, setInMeeting] = useState(true);//Change to JSON of next meeting
 
+  //TODO: Add text input which will be sent to server on submit
+  //TODO: Figure out how to display the messages that are being saved
   useEffect(() => {
     if (socketOpen) {
       socket.emit(
@@ -15,16 +19,24 @@ export default function Active({ socket, socketOpen }) {
     }
   }, [socket, socketOpen]);
 
-  let myImage = new Image();
-  myImage.onload = () => { setLoaded(true) };
-  myImage.src = theImage;
+  if (inMeeting) {
+    let myImage = new Image();
+    myImage.onload = () => { setLoaded(true) };
+    myImage.src = theImage;
 
-  return (
-
-    <div id="blah">
-      <ImageCanvas myImage={myImage} isLoaded={isLoaded} />
-      <Canvas />
-    </div>
-
-  );
+    return (
+      <div id="canvas-container">
+        <ImageCanvas myImage={myImage} isLoaded={isLoaded} />
+        <Canvas />
+      </div>
+    )
+  } else {
+    return (
+      <>
+        <h1> No active meeting. </h1>
+        <p> The next schedule meeting is [ ... ]</p>
+        <img src="images/no_meeting.jpg" alt="Sad Pikachu" />
+      </>
+    )
+  }
 }
