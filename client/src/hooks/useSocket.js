fadff86bf81a5c1d857ace31aa265ed3a269d799
Tammie Chung <tammie.chung11@gmail.com>
2019-11-24 15:49:46 -0500

    Fix cookies bug - add user to active users

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -5,10 +5,10 @@ export const useSocket = () => {
   const [socket, setSocket] = useState(null);
   const [socketOpen, setSocketOpen] = useState(false);
 
-  // const server = 'localhost:8080'; // Local test
+  const server = 'localhost:8080'; // Local test
   // const server = '172.46.3.253:8080'; //Owen
   // const server = '172.46.3.232:8080'; //TH
-  const server = '172.46.0.146:8080'; //Tammie
+  // const server = '172.46.0.146:8080'; //Tammie
 
   useEffect(() => {
     const s = io(server);
