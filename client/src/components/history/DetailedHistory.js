f5f11b350b46e94278f449149517a028ad3386a3
Tammie Chung <tammie.chung11@gmail.com>
2019-11-25 14:53:05 -0500

    Add copy to clipboard feature

--- client/src/components/history/DetailedHistory.js
+++ client/src/components/history/DetailedHistory.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 
 export default function DetailedHistory(props) {
 
   console.log('props for details', props);
+
+  const notesRef = useRef(null);
 
   const [notes, setNotes] = useState('');
   const [image, setImage] = useState('');
@@ -17,7 +20,13 @@ export default function DetailedHistory(props) {
     });
 
     return () => props.socket.off('notes');
-  }, [props.socket, props.meeting.id, props.user, props.meeting.link_to_final_doc])
+  }, [props.socket, props.meeting.id, props.user, props.meeting.link_to_final_doc]);
+
+  const copyToClipboard = () => {
+    console.log(notesRef.current.value)
+    notesRef.current.select();
+    document.execCommand('copy');
+  }
 
   return (
     <Box>
@@ -29,8 +38,11 @@ export default function DetailedHistory(props) {
       <p>{props.meeting.invited_users.map((name, index) => <span key={index}>{name} </span>)}</p>
       <h4>Description</h4>
       <p>{props.meeting.description}</p>
-      <h4>My Notes</h4>
-      <p>{notes}</p>
+      <div className='personal-notes'>
+        <h4>My Notes</h4>
+        <textarea className='notes-text' ref={notesRef} value={notes}>{notes}</textarea>
+        <FileCopyIcon onClick={copyToClipboard} />
+      </div>
       <h4>Group Notes</h4>
       <img className='meeting-image' src={image} alt='meeting-notes' />
       <Button variant="contained" onClick={() => props.setViewMeeting(0)}>Back</Button>
