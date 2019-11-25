cabf0ac8f19dd14b32b28ab3a6c083322e7f5b43
Owen Ripley <ojripley@gmail.com>
2019-11-25 07:36:43 -0500

    make db relations cascade on delete

--- backend/db/schema/04_users_meetings.sql
+++ backend/db/schema/04_users_meetings.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS users_meetings CASCADE;
 CREATE TABLE users_meetings (
   id SERIAL PRIMARY KEY NOT NULL,
-  meeting_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
+  meeting_id INTEGER REFERENCES meetings(id) ON DELETE CASCADE NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
   notes text,
   attendance VARCHAR(255)
 );
