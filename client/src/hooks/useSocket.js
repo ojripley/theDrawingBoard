4a1ba17eba1c70b84cb294683ad7270a32bd38eb
Thilakshan <th_thilakshan@hotmail.com>
2019-11-26 09:28:48 -0500

    change ip

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -5,8 +5,8 @@ export const useSocket = () => {
   const [socket, setSocket] = useState(null);
   const [socketOpen, setSocketOpen] = useState(false);
 
-  const server = '172.46.3.253:8080'; //Owen
-  // const server = '172.46.3.232:8080'; //TH
+  // const server = '172.46.3.253:8080'; //Owen
+  const server = '172.46.3.232:8080'; //TH
   // const server = '172.46.0.146:8080'; //Tammie
 
   useEffect(() => {
