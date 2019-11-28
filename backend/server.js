6d81cdfdbe83e6e32730c56da0679f8664aad63f
Thilakshan <th_thilakshan@hotmail.com>
2019-11-28 09:52:57 -0500

    fix server to accept multiple files

--- backend/server.js
+++ backend/server.js
@@ -274,7 +274,7 @@ io.on('connection', (client) => {
   });
 
   client.on('insertMeeting', data => {
-    db.insertMeeting(data.startTime, data.ownerId, data.name, data.description, data.status, data.file.name)
+    db.insertMeeting(data.startTime, data.ownerId, data.name, data.description, data.status, "image.png") //we no longer need the link_to_initial_doc column
       .then(res => {
         client.emit('newMeeting', res[0]);
         // console.log(res[0].id);
@@ -282,36 +282,38 @@ io.on('connection', (client) => {
       })
       .then((id) => {
         fs.mkdir(`meeting_files/${id}`, () => { //makes a new directory for the meeting
-          console.log(data.filename);
-          if (data.file.name) { //Only save the file if one exists, otherwise just have an empty folder
-            //Check if pdf
-            if (data.file.name.search(/\.pdf$/ig) !== -1) {
+          for (let file of data.files) {
+            if (file.name) { //Only save the file if one exists, otherwise just have an empty folder
+              //Check if pdf
+              if (file.name.search(/\.pdf$/ig) !== -1) {
 
-              console.log(data.file.name);
-              fs.writeFile(`meeting_files/${id}/${data.file.name}`, data.file.payload, (err) => {
-                if (err) {
-                  console.log('problem');
-                  throw err;
-                }
-                console.log('The file has been saved!');
+                console.log(file.name);
+                fs.writeFile(`meeting_files/${id}/${file.name}`, file.payload, (err) => {
+                  if (err) {
+                    console.log('problem');
+                    throw err;
+                  }
+                  console.log('The file has been saved!');
 
-                let pdfImage = new PDFImage(`meeting_files/${id}/${data.file.name}`);
+                  let pdfImage = new PDFImage(`meeting_files/${id}/${file.name}`);
 
-                pdfImage.convertFile().then(function(imagePath) {
-                  // 0-th page (first page) of the slide.pdf is available as slide-0.png
-                  console.log(imagePath);
-                })
-                  .catch((error) => {
-                    console.log(error);
+                  pdfImage.convertFile().then(function(imagePath) {
+                    // 0-th page (first page) of the slide.pdf is available as slide-0.png
+                    console.log(imagePath);
                   })
-              }); //Note promisy this I if we want to wait for the upload to finish before creating meeting
-            } else {
-              fs.writeFile(`meeting_files/${id}/${data.file.name}`, data.file.payload, (err) => {
-                if (err) throw err;
-                console.log('The file has been saved!');
-              }); //Note promisy this I if we want to wait for the upload to finish before creating meeting
+                    .catch((error) => {
+                      console.log(error);
+                    })
+                }); //Note promisy this I if we want to wait for the upload to finish before creating meeting
+              } else {
+                fs.writeFile(`meeting_files/${id}/${file.name}`, file.payload, (err) => {
+                  if (err) throw err;
+                  console.log('The file has been saved!');
+                }); //Note promisy this I if we want to wait for the upload to finish before creating meeting
+              }
             }
           }
+          console.log(filename);
         });
         const promiseArray = [];
 
