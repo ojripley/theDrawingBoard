0fda63b3251b80a48846a26722e40e4194985d70
Owen Ripley <ojripley@gmail.com>
2019-11-28 17:32:28 -0500

    change to reboot heroku

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ const db = require('./db/queries/queries');
 // CORS
 app.use(cors());
 
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 // The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -160,6 +161,7 @@ io.on('connection', (client) => {
 
       } catch (err) {
         console.error('Cookie authentication failed!');
+        console.log('test');
       }
     } else {
       client.emit('cookieResponse', null);
