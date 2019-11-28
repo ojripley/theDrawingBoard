92c06290674bb7efe3b7735fb12d7cc8cf40edc8
Thilakshan <th_thilakshan@hotmail.com>
2019-11-28 09:40:55 -0500

    remove unused code

--- backend/server.js
+++ backend/server.js
@@ -297,14 +297,8 @@ io.on('connection', (client) => {
 
                 let pdfImage = new PDFImage(`meeting_files/${id}/${data.file.name}`);
 
-                // console.log(pdfImage);
-                // var pdfImage2 = new PDFImage(`meeting_files/${id}/${data.file.name}`);
-                // pdfImage2.convertFile().then(function(imagePaths) {
-                //   // [ /tmp/slide-0.png, /tmp/slide-1.png ]
-                // });
                 pdfImage.convertFile().then(function(imagePath) {
                   // 0-th page (first page) of the slide.pdf is available as slide-0.png
-                  fs.existsSync("/tmp/slide-0.png") // => true\
                   console.log(imagePath);
                 })
                   .catch((error) => {
