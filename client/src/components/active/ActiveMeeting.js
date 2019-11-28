60cd17ace361ca579c0cb4685760a9f7d61eb9f5
Owen Ripley <ojripley@gmail.com>
2019-11-27 19:25:42 -0500

    remove dead code

--- client/src/components/active/ActiveMeeting.js
+++ client/src/components/active/ActiveMeeting.js
@@ -13,7 +13,6 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 import CanvasDrawer from './CanvasDrawer';
 
-
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
@@ -60,25 +59,33 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-// debugging
-var connSender = {};
-var connReceiver = {};
-
 export default function ActiveMeeting({ socket, socketOpen, initialNotes, user, meetingId, setInMeeting, ownerId, setMeetingId, setMode, imageLoaded, backgroundImage, initialPixels }) {
 
+  const classes = useStyles();
+
+  // const [imageLoaded, setLoaded] = useState(false);
+  const [meetingNotes, setMeetingNotes] = useState(initialNotes || '');
+  const [writeMode, setWriteMode] = useState(false);
+  const [saving, setSaving] = useState(true);
+  const debouncedNotes = useDebounce(meetingNotes, 400);
+
+  const textareaRef = useRef(null);
+
+  // webrtc related state
   const { peer, setPeer } = usePeer();
   const { connections, setConnections } = useConnection();
   const [newPeer, setNewPeer] = useState(null);
-  const [iAmInitiator, setIAmInitiator] = useState(null);
+  const [isInitiator, setIsInitiator] = useState(null);
   // const connections = useRef(null);
 
   useEffect(() => {
+    // assign the user a Peer object when they join the meeting
     setPeer(new Peer(String(user.id), {key: 'peerjs'}));
-    // connections.current = {};
   }, []);
 
   useEffect(() => {
-    console.log('useEffect #2 ran');
+
+    // make sure the user has been assigned a Peer object
     if (peer) {
       console.log('i am peer', peer);
 
@@ -92,33 +99,13 @@ export default function ActiveMeeting({ socket, socketOpen, initialNotes, user, 
         console.log(conn.peer);
         const peerId = conn.peer;
 
-        // debugging
-        connReceiver = conn;
-        window.receiver = conn;
-
-        // setting the state needing for opening connections in third useEffect
-        setIAmInitiator(false);
+        // setting the state needing for opening connections in useEffect
+        setIsInitiator(false);
         setNewPeer(peerId);
         setConnections(prev => ({
           ...prev,
           [peerId]: conn
         }));
-
-        // connections.current[peerId] = conn;
-
-        // connections.current[peerId].on('open', () => {
-        //   connections.current[peerId].on('data', (data) => {
-        //     console.log(data);
-        //   });
-        //   connections.current[peerId].send('test');
-        // });
-
-        // connReceiver.on('open', () => {
-        //   connReceiver.on('data', (data) => {
-        //     console.log(data);
-        //   });
-        //   connReceiver.send('test');
-        // });
       });
 
       if (socketOpen) {
@@ -127,7 +114,6 @@ export default function ActiveMeeting({ socket, socketOpen, initialNotes, user, 
 
           // log who that is
           console.log('new user', data.user.username);
-
 
           // make sure it isn't yourself
           if (data.user.id !== user.id) {
@@ -139,36 +125,13 @@ export default function ActiveMeeting({ socket, socketOpen, initialNotes, user, 
             console.log('i am trying to start a call with', peerId);
             const conn = peer.connect(String(peerId));
 
-            // debugging
-            window.initiator = conn;
-            connSender = conn;
-
             // setting the state needing for opening connections in third useEffect
-            setIAmInitiator(true);
+            setIsInitiator(true);
             setNewPeer(peerId);
             setConnections(prev => ({
               ...prev,
               [peerId]: conn
             }));
-
-            // connections.current[peerId] = conn;
-
-            // connections.current[peerId].on('open', () => {
-            //   console.log('connection open');
-            //   connections.current[peerId].send('test');
-            //   connections.current[peerId].on('data', (data) => {
-            //     console.log(data);
-            //   });
-            // });
-
-            // console.log(conn);
-            // connSender.on('open', () => {
-            //   console.log('connection open');
-            //   connSender.send('test');
-            //   connSender.on('data', (data) => {
-            //     console.log(data);
-            //   });
-            // });
           }
         });
       }
@@ -176,343 +139,42 @@ export default function ActiveMeeting({ socket, socketOpen, initialNotes, user, 
     return () => {
       socket.off('newParticipant');
     }
-
   }, [peer]);
 
   useEffect(() => {
-    console.log('connections changed');
 
+    // if user is a peer and a new connection has been made with a newPeer
     if (newPeer && peer) {
-      console.log('newPeer', newPeer);
-      console.log(connections);
-      console.log(connections[newPeer]);
-      if (iAmInitiator) {
+
+      // if the user is the initiator of the connection
+      if (isInitiator) {
+
+        // set a listener for when their initiated P2P connection has been opened
         connections[newPeer].on('open', () => {
+
           console.log('connection open');
-          connections[newPeer].send('test');
+          connections[newPeer].send(`Hello from peer ${user.username}`);
+
+          // open a listener to recieve data from the peers
           connections[newPeer].on('data', (data) => {
             console.log(data);
           });
         });
 
-      } else {
+      } else { // if the user is the receiver of the connection
+
+        // set a listener for when the received P2P connection has been opened
         connections[newPeer].on('open', () => {
+
+          // set a listener for recieving data from the peer
           connections[newPeer].on('data', (data) => {
             console.log(data);
           });
-          connections[newPeer].send('test');
+          connections[newPeer].send(`Hello from peer ${user.username}`);
         });
       }
     }
-
   }, [connections]);
-
-  // useEffect(() => {
-  //   console.log(connections.current);
-  // }, [connections.current]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // useEffect(() => {
-  //   if (socketOpen) {
-  //     // when someone else joins the meeting
-  //     socket.on('newParticipant', (data) => {
-
-  //       // log who that is
-  //       console.log('new user', data.user.username);
-
-  //       // make sure it isn't yourself
-  //       if (data.user.id !== user.id) {
-
-  //         // start a call with them
-
-  //       }
-  //     });
-  //   }
-
-  //   return () => {
-  //     socket.off('newParticipant');
-  //   }
-  // }, [socket, socketOpen, peer, connections]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // useEffect(() => {
-  //   console.log('i am', user.id);
-  //   if (ownerId !== user.id) {
-  //     const peer = new Peer('tc');
-  //     console.log('i am going to start a connection');
-  //     // setPeers(prev => ({
-  //     //   ...prev,
-  //     //   peerId: new Peer(ownerId)
-  //     // }));
-
-
-
-
-
-
-  //     // const connection = peers[ownerId].connect(ownerId);
-  //     const connection = peer.connect('oj');
-
-  //     console.log('connection', connection);
-
-  //     connection.on('open', () => {
-  //       console.log('connection is opened');
-
-  //       connection.on('data', (data) => {
-  //         console.log('incoming', data);
-  //       });
-
-  //       connection.send('test');
-  //     });
-
-
-
-
-
-
-  //     // peer.on('connection', (conn) => {
-  //     //   conn.send('test');
-  //     //   // listen for data
-  //     //   conn.on('data', (data) => {
-  //     //     console.log(data);
-  //     //   });
-  //     // });
-  //   }
-  // }, []);
-
-
-  // useEffect(() => {
-  //   if (socketOpen) {
-  //     // when someone else joins the meeting
-  //     socket.on('newParticipant', (data) => {
-
-  //       // log who that is
-  //       console.log('new user', data.user.username);
-
-  //       // make sure it's yourself
-  //       if (ownerId === user.id && user.id === data.user.id) {
-  //         // setConnection(peer.connect(data.user.id));
-
-  //         const peerId = user.id;
-  //         // let peer = peers[peerId];
-  //         console.log(user.id);
-
-  //         // if user does not yet regard new participant as a peer
-  //         // if (!peers[peerId]) {
-  //         //   // create a peer
-  //         //   // peer = new Peer('oj', { key: 'lwjd5qra8257b9' });
-  //         //   setPeers(prev => ({
-  //         //     ...prev,
-  //         //     peerId: new Peer(peerId)
-  //         //   }));
-  //         // }
-
-  //         peer.on('open', (id) => {
-  //           console.log('connected to peerjs server... my id is');
-  //           console.log(id);
-  //         })
-
-  //         // // listen for connections
-  //         // peer.on('connection', (connection) => {
-
-  //         //   // listen for data
-  //         //   connection.on('data', (data) => {
-  //         //     console.log(data);
-  //         //   });
-  //         // });
-
-  //         // const connection = peer.connect(peerId);
-  //         // if a connection does not yet exist with this peer
-  //         // if (!connections[peerId]) {
-  //         //   // make a connection
-  //         //   console.log(connection);
-  //         //   setConnections(prev => ({
-  //         //     ...prev,
-  //         //     peerId: connection
-  //         //   }));
-
-  //         //   console.log(connections);
-  //         // }
-
-  //         // get ready to log whatever data they send when connection is complete
-  //         peer.on('connection', (conn) => {
-  //           console.log('connected but not open');
-  //           conn.on('open', () => {
-  //             console.log('open connection', conn);
-
-
-  //             conn.on('data', (data) => {
-  //               console.log(data);
-  //             });
-
-  //             conn.send('test');
-  //           });
-  //         });
-  //       }
-  //     });
-  //   }
-
-  //   return () => {
-  //     socket.off('newParticipant');
-  //   }
-  // }, [socket, socketOpen, peer, connections]);
-
-
-
-
-
-
-
-
-
-
-
-  // const test = new Peer(user.id);
-
-  // console.log(test);
-  // console.log(new Peer(user.id));
-
-  const classes = useStyles();
-
-  // const [imageLoaded, setLoaded] = useState(false);
-  const [meetingNotes, setMeetingNotes] = useState(initialNotes || '');
-  const [writeMode, setWriteMode] = useState(false);
-  const [saving, setSaving] = useState(true);
-  const debouncedNotes = useDebounce(meetingNotes, 400);
-  // const backgroundCanvas = useRef(null);
-
-  // webrtc state
-  // setting connection state always seems to be null...
-  // const [peer, setPeer] = useState(new Peer(user.id));
-  // const [peer, setPeer] = useState(null);
-  // const [connection, setConnection] = useState(null);
-  // const [call, setCall] = useState(null);
-
-  const textareaRef = useRef(null);
-  // const peer = new Peer(user.id);
-
-  // useEffect doesn't seem to work with peer connection
-  // useEffect(() => {
-
-  //   peer.on('connection', (conn) => {
-  //     conn.on('data', (data) => {
-  //       console.log(data);
-  //     })
-  //   });
-
-  // }, []);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // peer.on('connection', (connection) => {
-  //   console.log('new connection', connection);
-  //   console.log(connection);
-  //   // const conn = peer.connect(connection.peer);
-  //   connection.on('data', (data) => {
-  //     console.log('connection data', data);
-  //   });
-  // });
 
   const handleInput = (e) => {
     console.log(e.target.value)
@@ -525,56 +187,6 @@ export default function ActiveMeeting({ socket, socketOpen, initialNotes, user, 
     e.target.value = ''
     e.target.value = temp_value
   }
-
-  // useEffect(() => {
-  //   peer.on('connection', (connection) => {
-  //     connection.on('data', (data) => {
-  //       console.log(data);
-  //     })
-  //   });
-
-  //   return () => {
-  //     peer.off('connection');
-  //   }
-  // }, [peer]);
-
-  // useEffect(() => {
-  //   if (socketOpen) {
-  //     // when someone else joins the meeting
-  //     socket.on('newParticipant', (data) => {
-
-  //       // log who that is
-  //       console.log(data.user.username);
-
-  //       // make sure it's not yourself
-  //       if (data.user.id !== user.id) {
-  //         // setConnection(peer.connect(data.user.id));
-
-  //         // make a connection with them
-  //         const connection = peer.connect(data.user.id);
-  //         console.log('connection', connection);
-
-  //         // get ready to log whatever data they send when connection is complete
-  //         connection.on('open', () => {
-  //           console.log('open connection');
-  //           connection.send('test');
-  //         });
-  //       }
-  //     });
-  //   }
-
-  //   return () => {
-  //     socket.off('newParticipant');
-  //   }
-  // }, [socket, socketOpen]);
-
-  // useEffect(() => {
-  //   // setPeer(new Peer(user.id));
-  //   console.log(peer);
-
-  //   setCall(peer)
-
-  // }, [peer]);
 
   useEffect(() => {
     socket.on('requestNotes', res => {
@@ -592,10 +204,8 @@ export default function ActiveMeeting({ socket, socketOpen, initialNotes, user, 
 
   useEffect(() => {
     if (socketOpen) {
-      // console.log(debouncedNotes);
       console.log('requesting note save');
       socket.emit('saveDebouncedNotes', { user: user, meetingId: meetingId, notes: debouncedNotes });
-      // socket.on('receiveOkay') //can have a socket on when received
       setSaving(false);
     }
   }, [socket, socketOpen, debouncedNotes, user, meetingId])
