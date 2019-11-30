1fff499ba934f06b40b9aaed71b9520691a601ac
Tammie Chung <tammie.chung11@gmail.com>
2019-11-30 13:15:33 -0500

    Clean up dead code

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -6,84 +6,8 @@ import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-// import cookie from "react-cookie";
 
 import './Login.scss';
-
-// export default function Login(props) {
-
-//   const [password, setPassword] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [showPassword, setShowPassword] = useState(false);
-
-//   const handleLogin = () => {
-//     if (props.socketOpen) {
-//       console.log('attempting to log in')
-//       console.log(email, password)
-//       props.socket.emit('loginAttempt', { email: email, password: password });
-//     }
-//   };
-
-//   const onEnter = event => {
-//     if (event.charCode === 13) {
-//       handleLogin();
-//     }
-//   }
-
-//   useEffect(() => {
-//     if (props.socketOpen) {
-//       props.socket.on('loginResponse', (data) => {
-//         if (data.user && data.user.id) {
-//           console.log(data);
-//           console.log("Attempting to set cookie");
-//           document.cookie = `sid=${data.session.sid}`
-//           document.cookie = `iv=${data.session.iv}`;
-//           console.log(document.cookie);
-//           props.setUser(data.user);
-//         }
-//       })
-
-//       return () => props.socket.off('loginResponse')
-//     }
-//   });
-
-//   return (
-//     <div id='app-container'>
-//       <Typography id='page-header' variant='h2' color='primary'>Login</Typography>
-//       <Typography variant='h2' color='primary'>Register</Typography>
-//       <Divider color='primary' />
-//       <TextField
-//         id="email"
-//         label="Email"
-//         color="secondary"
-//         value={email}
-//         onChange={event => setEmail(event.target.value)}
-//       />
-//       <TextField
-//         id="password"
-//         label="Password"
-//         color="secondary"
-//         type={showPassword ? 'text' : 'password'}
-//         value={password}
-//         onChange={event => setPassword(event.target.value)}
-//         onKeyPress={onEnter}
-//         InputProps={{
-//           endAdornment:
-//             <InputAdornment position="end">
-//               <IconButton
-//                 aria-label="toggle password visibility"
-//                 onClick={() => showPassword ? setShowPassword(false) : setShowPassword(true)}
-//               >
-//                 {showPassword ? <Visibility /> : <VisibilityOff />}
-//               </IconButton>
-//             </InputAdornment>
-//         }}
-//       />
-//       <Button variant="contained" onClick={handleLogin}>Login</Button>
-//     </div>
-//   );
-// }
 
 export default function Login(props) {
 
