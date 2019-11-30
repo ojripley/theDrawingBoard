0e62f38519896f6f84b8a1b06eba6cbda5830ee2
Thilakshan <th_thilakshan@hotmail.com>
2019-11-30 11:27:34 -0500

    fix bug where crash occurs when user changes pages while another is drawing

--- client/src/reducers/canvasReducer.js
+++ client/src/reducers/canvasReducer.js
@@ -73,27 +73,20 @@ export default function reducer(state, action) {
         for (let i in pixels) {
           state.ctx.beginPath(); //start drawing a single line
           if (pixels[i].tool === "highlighter") {
-            // console.log("lighting")
-            // state.ctx.globalAlpha = 0.2;
             state.ctx.lineCap = 'butt';
             state.ctx.strokeStyle = highlightCol;
           } else {
-            // console.log("not lighting")
             state.ctx.lineCap = 'round';
             state.ctx.strokeStyle = col;
           }
           state.ctx.lineWidth = pixels[i].strokeWidth || 1;
-          // pixels[i].highlighting ? console.log("HIIII") : console.log("LOOOO");
-          // console.log(state.color[user])
-          if (pixels[i].dragging && i) { //if we're in dragging mode, use the last pixel
+
+          if (pixels[i].dragging && pixels[i - 1]) { //if we're in dragging mode, use the last pixel
             state.ctx.moveTo(pixels[i - 1].x * w, pixels[i - 1].y * h);
           } else { //else use the current pixel, offset by 1px to the left
             state.ctx.moveTo(pixels[i].x * w, pixels[i].y * h - 1);
           }
           state.ctx.lineTo(pixels[i].x * w, pixels[i].y * h);//draw a line from point mentioned above to the current pixel
-          // state.ctx.save();
-          // state.ctx.fillRect(pixels[i].x * w, pixels[i].y * h, 10, 10); // fill in the pixel at (10,10)
-
           state.ctx.stroke();//draw the line
           state.ctx.closePath();//end the line
         }
@@ -101,7 +94,6 @@ export default function reducer(state, action) {
       for (let user in state.pointers) {
         if (state.pointers[user] && state.pointers[user].x) {
           state.ctx.beginPath();
-          // state.ctx.arc(state.pointers[user].x * w, state.pointers[user].y * h, 20, 0, 2 * Math.PI);//center, r, stangle, endangle
           let x = state.pointers[user].x * w;
           let y = state.pointers[user].y * h;
           let r = state.pointers[user].strokeWidth / 2;
@@ -139,26 +131,20 @@ export default function reducer(state, action) {
         for (let i in pixels) {
           ctx.beginPath(); //start drawing a single line
           if (pixels[i].tool === "highlighter") {
-            // console.log("lighting")
-            // ctx.globalAlpha = 0.2;
             ctx.lineCap = 'butt';
             ctx.strokeStyle = highlightCol;
           } else {
-            // console.log("not lighting")
             ctx.lineCap = 'round';
             ctx.strokeStyle = col;
           }
           ctx.lineWidth = pixels[i].strokeWidth || 1;
-          // pixels[i].highlighting ? console.log("HIIII") : console.log("LOOOO");
-          // console.log(state.color[user])
+
           if (pixels[i].dragging && i) { //if we're in dragging mode, use the last pixel
             ctx.moveTo(pixels[i - 1].x * w, pixels[i - 1].y * h);
           } else { //else use the current pixel, offset by 1px to the left
             ctx.moveTo(pixels[i].x * w, pixels[i].y * h - 1);
           }
           ctx.lineTo(pixels[i].x * w, pixels[i].y * h);//draw a line from point mentioned above to the current pixel
-          // ctx.save();
-          // ctx.fillRect(pixels[i].x * w, pixels[i].y * h, 10, 10); // fill in the pixel at (10,10)
 
           ctx.stroke();//draw the line
           ctx.closePath();//end the line
