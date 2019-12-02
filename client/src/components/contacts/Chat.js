27f4b185c6021d95875972ab252bb3878c46eeaa
Owen Ripley <ojripley@gmail.com>
2019-12-02 17:06:17 -0500

    add server reemit notification

--- client/src/components/contacts/Chat.js
+++ client/src/components/contacts/Chat.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState, useRef }from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 import Message from '../Message';
 
+const useStyles = makeStyles(theme => ({
+  drawerContainer: {
+    backgroundColor: 'rgba(245,240,235, 0.85)'
+    // backgroundColor: theme.palette.primary.light
+  },
+  list: {
+    width: '40vw',
+    minWidth: 280,
+  },
+  // center: {
+  //   display: 'flex',
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  //   position: 'relative',
+  //   zIndex: 2,
+  //   width: '100%',
+  // },
+  textareaAutosize: {
+    resize: 'none',
+    width: '85%',
+    marginRight: 0,
+    borderRadius: '15px 15px',
+    border: 'none',
+    padding: '0.5em 0.75em 0'
+  },
+  button: {
+    zIndex: 999,
+    position: 'fixed',
+    top: 10,
+    right: 10
+  },
+  endButton: {
+    position: 'relative',
+    float: 'right',
+    margin: '0.5em 10px'
+  },
+  sendButton: {
+    position: 'relative',
+    padding: 0
+  },
+}));
+
 export default function Chat(props) {
+  console.log('props', props);
+  const classes = useStyles();
 
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
@@ -11,6 +62,15 @@ export default function Chat(props) {
   const textareaRef = useRef(null);
   const messagesDisplayRef = useRef(null);
 
+  const scrollToBottom = () => {
+    // messagesDisplayRef.current.scrollTop = messagesDisplayRef.current.scrollHeight;
+    messagesDisplayRef.current.scrollTo({
+      top: messagesDisplayRef.current.scrollHeight,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }
+
   const handleMessage = event => {
     setMessage(event.target.value);
   };
@@ -18,7 +78,8 @@ export default function Chat(props) {
   const handleMessageSend = () => {
     if (message.trim().length > 0) {
       if (props.socketOpen) {
-        props.socket.emit('sendDm', { user: props.user, recipientId: props.recipientId, msg: message.trim(), timestamp: Date.now()});
+        console.log('props', props);
+        props.socket.emit('sendDm', { user: props.user, recipientId: props.recipientId, msg: message.trim(), time: new Date(Date.now())});
       }
       console.log('unreadMessages for sender:', unreadMessages);
       setMessage('');
@@ -58,27 +119,41 @@ export default function Chat(props) {
         props.socket.off('dm');
       }
     }
-  }, [messages, props.socket, props.socketOpen, openDrawer, unreadMessages]);
+  }, [messages, props.socket, props.socketOpen, unreadMessages]);
+
+
+  const msgs = messages.map((message) => {
+    return (
+      <Message
+        key={message.time}
+        sender={message.user}
+        user={props.user}
+        msg={message.msg}
+        time={message.time}
+      />
+    )
+  });
+
 
   return (
 
-  <List id='meeting-chat-container'>
-    <div ref={messagesDisplayRef} id='messages-display'>{msgs}</div>
-    <ListItem id={`meeting-chat`}>
-      <TextareaAutosize
-        ref={textareaRef}
-        aria-label='empty textarea'
-        placeholder='Message'
-        value={message}
-        className={classes.textareaAutosize}
-        onFocus={handleCaret}
-        onChange={handleMessage}
-        onKeyPress={handleKeyStroke}
-        rows='2'
-        rowsMax='2'
-      />
-      <Button variant='text' color='primary' className={classes.sendButton} onClick={handleMessageSend}>Send</Button>
-    </ListItem>
-  </List>
+    <List id='meeting-chat-container'>
+      <div ref={messagesDisplayRef} id='messages-display'>{msgs}</div>
+      <ListItem id={`meeting-chat`}>
+        <TextareaAutosize
+          ref={textareaRef}
+          aria-label='empty textarea'
+          placeholder='Message'
+          value={message}
+          className={classes.textareaAutosize}
+          onFocus={handleCaret}
+          onChange={handleMessage}
+          onKeyPress={handleKeyStroke}
+          rows='2'
+          rowsMax='2'
+        />
+        <Button variant='text' color='primary' className={classes.sendButton} onClick={handleMessageSend}>Send</Button>
+      </ListItem>
+    </List>
   )
 }
