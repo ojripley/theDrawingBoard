d1482e3bd2a2e6dc52a5a16941e625a7cba1041e
Tammie Chung <tammie.chung11@gmail.com>
2019-12-04 08:56:38 -0500

    remove console logs from login

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -22,8 +22,6 @@ export default function Login(props) {
   const handleLogin = () => {
     if (props.socketOpen) {
       props.setLoginError(null);
-      console.log('attempting to log in')
-      console.log(email, password)
       props.socket.emit('loginAttempt', { email: email.trim().toLowerCase(), password: password.trim() });
     }
   };
@@ -31,8 +29,6 @@ export default function Login(props) {
   const handleRegister = () => {
     if (props.socketOpen) {
       props.setLoginError(null);
-      console.log('attempting to register');
-      console.log(username, email, password, confirmPassword);
       if (username.length > 0 && email.length > 0 && password.length > 0) {
         if (password === confirmPassword) {
           props.socket.emit('registrationAttempt', { username: username.replace(/\s/g,''), email: email.trim().toLowerCase(), password: password.trim() });
@@ -65,17 +61,13 @@ export default function Login(props) {
     if (props.socketOpen) {
       props.socket.on('loginResponse', (data) => {
         if (data.user && data.user.id) {
-          console.log(data);
-          console.log("Attempting to set cookie");
           document.cookie = `sid=${data.session.sid}`
           document.cookie = `iv=${data.session.iv}`;
-          console.log(document.cookie);
           props.setUser(data.user);
         }
       });
 
       props.socket.on('WelcomeYaBogeyBastard', res => {
-        console.log('registration success! logging in now', res);
         props.socket.emit('loginAttempt', { email: res.email, password: password })
       })
 
