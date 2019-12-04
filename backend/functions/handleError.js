0e5a36d59e55f185cecd852c2cbf9fcf0663386c
Owen Ripley <ojripley@gmail.com>
2019-12-04 10:15:38 -0500

    add comments to handleError.js

--- backend/functions/handleError.js
+++ backend/functions/handleError.js
@@ -2,10 +2,12 @@ const { generateClientError } = require('./generateClientError');
 
 const handleError = function(error, client) {
 
+  // log entire error to server console
   console.log('\n\n\nHear ye!\n\nGerald the Error Herald says:');
   console.log(error);
   console.log('\n\n\nGerald the Error Herald has spoken.');
 
+  // generate a concise error to send to the client
   const clientError = generateClientError(error);
 
   if (client) {
