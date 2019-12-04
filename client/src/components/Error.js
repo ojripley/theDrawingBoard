12366a717696eeb29971f977ceb1e2e5459aa186
Tammie Chung <tammie.chung11@gmail.com>
2019-12-04 09:33:49 -0500

    Style Gerald

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -7,19 +7,21 @@ import './Error.scss';
 export default function Error(props) {
 
   return (
-    <div id='error'>
-      <div id='error-msg'>
-        <Typography variant='h6' id='gerald'>
-          {'Gerald the Error Herald says:'}
-        </Typography>
-        <Typography variant='h2' id='title'>
-          {'Oops, ya broke it! :('}
-        </Typography>
-        <Typography variant='body1' id='msg'>
-          {props.error.msg}
-        </Typography>
+    <div id='error-container'>
+      <div id='error'>
+        <div id='error-msg'>
+          <Typography variant='h6' id='gerald'>
+            {'Gerald the Error Herald says:'}
+          </Typography>
+          <Typography variant='h2' id='title'>
+            {'Oops, ya broke it! :('}
+          </Typography>
+          <Typography variant='body1' id='msg'>
+            {props.error.msg}
+          </Typography>
+        </div>
+        <img src={geraldTheHerald} alt='Gerald the Error Herald'></img>
       </div>
-      <img src={geraldTheHerald} alt='Gerald the Error Herald'></img>
     </div>
   );
 }
