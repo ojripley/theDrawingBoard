a307ee99418f6caff76d8bb21519fae63e2e2358
Owen Ripley <ojripley@gmail.com>
2019-12-05 08:08:52 -0500

    m

--- client/src/components/dashboard/MeetingCard.js
+++ client/src/components/dashboard/MeetingCard.js
@@ -161,7 +161,9 @@ export default function MeetingCard({
                   if (++previousCount === data.images.length) {
                     setLoading(false);
                     setImageLoaded(true);
-                    setInMeeting(true);
+                    setTimeout(() => {
+                      setInMeeting(true);
+                    }, 1000);
                   }
                   return previousCount;
                 })
